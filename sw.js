27bfd42036d16dbdbe3953b8b41781e17b794ed5
milovnikmhd1234 <milovnikmhd1234@gmail.com>
2026-05-21 22:31:48 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Tento kód umožňuje aplikaci načítat data, i když je online
-  e.respondWith(fetch(e.request));
-});
